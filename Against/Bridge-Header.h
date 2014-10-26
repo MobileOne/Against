e87cd8868175fc4cbaf3ff13f6e9910bf30009b2
Frederic Baraillon <f.baraillon@gmail.com>
2014-10-26 20:38:57 +0100

    Facebook login
    Add FB SDK + Facebook login first view

---
+++ Against/Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridge-Header.h
+//  Against
+//
+//  Created by Frédéric BARAILLON on 26/10/2014.
+//  Copyright (c) 2014 Frédéric BARAILLON. All rights reserved.
+//
+
+#ifndef Against_Bridge_Header_h
+#define Against_Bridge_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
